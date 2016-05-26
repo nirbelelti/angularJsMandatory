f9d8ff32e3bf9b4c709742498563201b64ce350f
nirbelelti <nirbelelti@gmail.com>
2016-05-26 16:02:07 +0200

    Updated controller with update and delete functions

--- app/controllers/FormRegistrationCTRL.js
+++ app/controllers/FormRegistrationCTRL.js
@@ -6,6 +6,7 @@ angular.module("mandatory").
 controller("FormRegistrationCTRL",
     function($scope, $state, $resource, $http) {
     $scope.user={};
+        $scope.users = [];
 
         $scope.register = function() {
             //this function should just send to next view instead of submitting
@@ -17,7 +18,9 @@ controller("FormRegistrationCTRL",
                 .success(function(data) {
                     console.log(data);
 
-                    //$scope.$parent.dummyInternships.push($scope.internship);
+                    $scope.users.push($scope.user);
+
+
                     //delete the internship from the local array, dummyInternships.
 
                     $state.go("profile");
@@ -32,13 +35,85 @@ controller("FormRegistrationCTRL",
            url: "http://nodedb2.herokuapp.com/users/GetAll"})
             .success(function (data) {
              console.log(data);
-             $scope.user = data;
+             $scope.users = data;
 
 
     }).error(function (data) {
 
            });
+
+        $scope.deleteUser = function(user) {
+      console.log("delete user");
+
+            console.log(user);
+
+            $http({ method: "DELETE",
+                data: $scope.user,
+
+                url: "http://nodedb2.herokuapp.com/users/Delete/" + user._id })
+
+
+                .success(function(data) {
+
+                    console.log(user._id);
+
+                    for(var i = $scope.users.length - 1; i >= 0; i--) {
+                        if($scope.users[i]._id === user._id) {
+
+                            $scope.users.splice(i,1);
+                        }
+                    }
+                    console.log($scope.internship);
+                    console.log("iteme removed");
+                    alert("item  deleted");
+
+                    $state.go("profile")
+
+                }).error(function(data) {
+            });
+        };
+
+
+        $scope.updateUser = function(user) {
+
+            $http({
+                method: "PUT",
+                data: $scope.user,
+
+                url: "http://nodedb2.herokuapp.com/users/Update/" + user._id
+            })
+                .success(function (data) {
+
+
+                    for (var i = $scope.users.length - 1; i >= 0; i--) {
+                        if ($scope.users[i]._id === user._id) {
+
+
+                            $scope.users.push(i, $scope.user);
+                            $scope.users[i] = data;
+
+
+                        }
+                        //  console.log("i"+i);
+
+                    }
+
+                    console.log("update " + user._id);
+
+
+                    $state.go("profile");
+                }).error(function (data) {
+
+            });
+        };
+
+
+
+
+
+
     });
+
 
 
     var today = new Date();
