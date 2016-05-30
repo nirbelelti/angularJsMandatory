bc2a7776b95ca2696f758124f45b3945764e1c6b
velislav-p <v.peychev@yahoo.com>
2016-05-30 11:02:13 +0200

    if statement in the register function

--- app/controllers/FormRegistrationCTRL.js
+++ app/controllers/FormRegistrationCTRL.js
@@ -1,36 +1,102 @@
-/**
- * Created by fiejelved on 18/05/16.
- */
-
 angular.module("mandatory").
 controller("FormRegistrationCTRL",
     function($scope, $state, $resource, $http) {
-    $scope.user={};
-        //configured $resource
-        $scope.userResource =
-            $resource(
-                "http://nodedb2.herokuapp.com/users/getall", {id:"@id"}, {update: {method: 'PUT'}});
+        $scope.user={};
+        $scope.users = [];
 
-        //retrieve all users
-        $scope.userResource.query(
-            function(data) {
-                $scope.dummyUsers = data;
-            }, function(data) {
-                //something went wrong....
-            });
         $scope.register = function() {
-
-
+            if ($scope.regForm.$valid) {
 
             $http({ method: "POST",
                 data: $scope.user,
                 url: "http://nodedb2.herokuapp.com/users/Create"})
                 .success(function(data) {
                     console.log(data);
-                    $scope.$dummyUsers.push($scope.user);
+
+                    $scope.users.push($scope.user);
 
                     $state.go("profile");
                 }).error(function(data) {
+            });
+        }
+        };
+
+
+
+
+        $http({ method: "GET",
+            url: "http://nodedb2.herokuapp.com/users/GetAll"})
+            .success(function (data) {
+                console.log(data);
+                $scope.users = data;
+
+
+            }).error(function (data) {
+
+        });
+
+        $scope.deleteUser = function(user) {
+            console.log("delete user");
+
+            console.log(user);
+
+            $http({ method: "DELETE",
+                data: $scope.user,
+
+                url: "http://nodedb2.herokuapp.com/users/Delete/" + user._id })
+
+
+                .success(function(data) {
+
+                    console.log(user._id);
+
+                    for(var i = $scope.users.length - 1; i >= 0; i--) {
+                        if($scope.users[i]._id === user._id) {
+
+                            $scope.users.splice(i,1);
+                        }
+                    }
+
+                    console.log("iteme removed");
+                    alert("item  deleted");
+
+                    $state.go("profile")
+
+                }).error(function(data) {
+            });
+        };
+
+
+        $scope.updateUser = function(user) {
+
+            $http({
+                method: "PUT",
+                data: $scope.user,
+
+                url: "http://nodedb2.herokuapp.com/users/Update/" + user._id
+            })
+                .success(function (data) {
+
+
+                    for (var i = $scope.users.length - 1; i >= 0; i--) {
+                        if ($scope.users[i]._id === user._id) {
+
+
+                            $scope.users.push(i, $scope.user);
+                            $scope.users[i] = data;
+
+
+                        }
+                        //  console.log("i"+i);
+
+                    }
+
+                    console.log("update " + user._id);
+
+
+                    $state.go("profile");
+                }).error(function (data) {
+
             });
         };
 
@@ -38,15 +104,6 @@ controller("FormRegistrationCTRL",
 
 
 
+
     });
-    //$http({ method: "GET",
-    //    url: "http://nodedb.herokuapp.com/users/getAll" })
-    //    .success(function(data) {
-    //        console.log(data);
-    //        $scope.dummyUsers = data;
-    //
-    //
-    //    }).error(function(data) {
-    //
-    //    });
 
