e3827c3747e9e97592bf48711329f769b52fe6dc
fiejelved <fiejelved@gmail.com>
2016-05-30 14:23:10 +0200

    new validation to submit enter btn

--- app/controllers/FormRegistrationCTRL.js
+++ app/controllers/FormRegistrationCTRL.js
@@ -1,151 +1,159 @@
-/**
- * Created by fiejelved on 18/05/16.
- */
+    /**
+     * Created by fiejelved on 18/05/16.
+     */
 
-angular.module("mandatory").
-controller("FormRegistrationCTRL",
-    function($scope, $state, $resource, $http) {
-    $scope.user={};
-        $scope.users = [];
+    angular.module("mandatory").
+    controller("FormRegistrationCTRL",
+        function($scope, $state, $resource, $http) {
+        $scope.user={};
+            $scope.users = [];
 
-        $scope.register = function() {
-            //this function should just send to next view instead of submitting
+            $scope.register = function() {
+                //this function should just send to next view instead of submitting
+                $scope.regForm.$submitted=true;
 
-
-            $http({ method: "POST",
-                data: $scope.user,
-                url: "http://nodedb2.herokuapp.com/users/Create"})
-                .success(function(data) {
-                    console.log(data);
-
-                    $scope.users.push($scope.user);
-
-
-                    //delete the internship from the local array, dummyInternships.
-
-                    $state.go("profile");
-                }).error(function(data) {
-            });
-        };
-
-
-
-
-           $http({ method: "GET",
-           url: "http://nodedb2.herokuapp.com/users/GetAll"})
+    if($scope.regForm.$valid) {
+        console.log('register submit');
+        $http({
+            method: "POST",
+            data: $scope.user,
+            url: "http://nodedb2.herokuapp.com/users/Create"
+        })
             .success(function (data) {
-             console.log(data);
-             $scope.users = data;
+                console.log(data);
+
+                $scope.users.push($scope.user);
 
 
-    }).error(function (data) {
+                //delete the internship from the local array, dummyInternships.
 
-           });
-
-        $scope.deleteUser = function(user) {
-      console.log("delete user");
-
-            console.log(user);
-
-            $http({ method: "DELETE",
-                data: $scope.user,
-
-                url: "http://nodedb2.herokuapp.com/users/Delete/" + user._id })
+                $state.go("profile");
+            }).error(function (data) {
+        });
+    }else{
+        console.log('else?');
+        $scope.regForm.$invalid
+    }
+            };
 
 
-                .success(function(data) {
-
-                    console.log(user._id);
-
-                    for(var i = $scope.users.length - 1; i >= 0; i--) {
-                        if($scope.users[i]._id === user._id) {
-
-                            $scope.users.splice(i,1);
-                        }
-                    }
-                    console.log($scope.internship);
-                    console.log("iteme removed");
-                    alert("item  deleted");
-
-                    $state.go("profile")
-
-                }).error(function(data) {
-            });
-        };
 
 
-        $scope.updateUser = function(user) {
-
-            $http({
-                method: "PUT",
-                data: $scope.user,
-
-                url: "http://nodedb2.herokuapp.com/users/Update/" + user._id
-            })
+               $http({ method: "GET",
+               url: "http://nodedb2.herokuapp.com/users/GetAll"})
                 .success(function (data) {
+                 console.log(data);
+                 $scope.users = data;
 
 
-                    for (var i = $scope.users.length - 1; i >= 0; i--) {
-                        if ($scope.users[i]._id === user._id) {
+        }).error(function (data) {
+
+               });
+
+            $scope.deleteUser = function(user) {
+          console.log("delete user");
+
+                console.log(user);
+
+                $http({ method: "DELETE",
+                    data: $scope.user,
+
+                    url: "http://nodedb2.herokuapp.com/users/Delete/" + user._id })
 
 
-                            $scope.users.push(i, $scope.user);
-                            $scope.users[i] = data;
+                    .success(function(data) {
 
+                        console.log(user._id);
+
+                        for(var i = $scope.users.length - 1; i >= 0; i--) {
+                            if($scope.users[i]._id === user._id) {
+
+                                $scope.users.splice(i,1);
+                            }
+                        }
+                        console.log($scope.internship);
+                        console.log("iteme removed");
+                        alert("item  deleted");
+
+                        $state.go("profile")
+
+                    }).error(function(data) {
+                });
+            };
+
+
+            $scope.updateUser = function(user) {
+
+                $http({
+                    method: "PUT",
+                    data: $scope.user,
+
+                    url: "http://nodedb2.herokuapp.com/users/Update/" + user._id
+                })
+                    .success(function (data) {
+
+
+                        for (var i = $scope.users.length - 1; i >= 0; i--) {
+                            if ($scope.users[i]._id === user._id) {
+
+
+                                $scope.users.push(i, $scope.user);
+                                $scope.users[i] = data;
+
+
+                            }
+                            //  console.log("i"+i);
 
                         }
-                        //  console.log("i"+i);
 
-                    }
-
-                    console.log("update " + user._id);
+                        console.log("update " + user._id);
 
 
-                    $state.go("profile");
-                }).error(function (data) {
+                        $state.go("profile");
+                    }).error(function (data) {
 
-            });
-        };
+                });
+            };
 
 
 
 
 
 
-    });
+        });
 
 
 
-    var today = new Date();
+        var today = new Date();
 
-    //configured $resource
-    /*$scope.usersResource =
-    $resource(
-        "http://angularkea2.azurewebsites.net/api/internships/:id",
-        { id: "@id" },dfsaz<dfhydsa<sy
-        {
-            update: { method: 'PUT' }
-        }
-    );
+        //configured $resource
+        /*$scope.usersResource =
+        $resource(
+            "http://angularkea2.azurewebsites.net/api/internships/:id",
+            { id: "@id" },dfsaz<dfhydsa<sy
+            {
+                update: { method: 'PUT' }
+            }
+        );
 
-    //retrieve all users
-    $scope.usersResource.query(
-    function(data) {
-        $scope.dummyUsers = data;
-    }, function(data) {
-        //something went wrong....
-    });*/
+        //retrieve all users
+        $scope.usersResource.query(
+        function(data) {
+            $scope.dummyUsers = data;
+        }, function(data) {
+            //something went wrong....
+        });*/
 
 
 
-    //$http({ method: "GET",
-    //    url: "http://nodedb.herokuapp.com/users/getAll" })
-    //    .success(function(data) {
-    //        console.log(data);
-    //        $scope.dummyUsers = data;
-    //
-    //
-    //    }).error(function(data) {
-    //
-    //    });
+        //$http({ method: "GET",
+        //    url: "http://nodedb.herokuapp.com/users/getAll" })
+        //    .success(function(data) {
+        //        console.log(data);
+        //        $scope.dummyUsers = data;
+        //
+        //
+        //    }).error(function(data) {
+        //
+        //    });
 
--- app/directives.js
+++ app/directives.js
@@ -2,10 +2,14 @@
  * Created by fiejelved on 27/05/16.
  */
 
+    var app = angular.module('mandatory');
+
 app.directive('myEnter', function () {
     return function (scope, element, attrs) {
         element.bind("keydown keypress", function (event) {
+            console.log('it works?');
             if(event.which === 13) {
+                console.log('does it work?');
                 scope.$apply(function (){
                     scope.$eval(attrs.myEnter);
                 });
@@ -15,3 +19,38 @@ app.directive('myEnter', function () {
         });
     };
 });
+
+// Add image to your profile
+
+/*
+app.directive('myDirective', function (httpPostFactory) {
+    return {
+        restrict: 'A',
+        scope: true,
+        link: function (scope, element, attr) {
+
+            element.bind('change', function () {
+                var formData = new FormData();
+                formData.append('file', element[0].files[0]);
+                httpPostFactory('upload_image.php', formData, function (callback) {
+                    // recieve image name to use in a ng-src
+                    console.log(callback);
+                });
+            });
+
+        }
+    };
+});
+
+app.factory('httpPostFactory', function ($http) {
+    return function (file, data, callback) {
+        $http({
+            url: file,
+            method: "POST",
+            data: data,
+            headers: {'Content-Type': undefined}
+        }).success(function (response) {
+            callback(response);
+        });
+    };
+});*/
